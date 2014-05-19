b8585ee22ca1b138a6fb18366f4c9f1e4242643f
WilliamHartOliveira <william.hart.oliveira@gmail.com>
2014-05-19 13:19:49 -0300

    Commit do Projeto VS2012
    Commit do Projeto VS2012

---
+++ ControleFilas/ControleFilas/ControleFilas.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ControleFilas
+{
+    partial class ControleFilas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ControleFilas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "ControleFilas";
+            this.Text = "Controle Filas";
+            this.Load += new System.EventHandler(this.ControleFilas_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ControleFilas/ControleFilas/ControleFilas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleFilas
+{
+    public partial class ControleFilas : Form
+    {
+        public ControleFilas()
+        {
+            InitializeComponent();
+        }
+
+        private void ControleFilas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
